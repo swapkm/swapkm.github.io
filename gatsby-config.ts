9b9e4f79b3a096c8c20506d489650b80d70fc7a8
SwapKam <swapkx@gmail.com>
2023-10-08 22:26:11 +0530

    init

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,11 +3,8 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `SwapKam`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://swapkam.com`
   },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: ["gatsby-plugin-postcss", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
     resolve: 'gatsby-plugin-manifest',
